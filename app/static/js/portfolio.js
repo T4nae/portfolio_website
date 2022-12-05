cc78c39dabfe3f684d22d2f52ae750e138478a36
T4nae <tan.b63000@gmail.com>
2022-12-05 12:45:44 +0530

    javascript for porfolio page

---
+++ app/static/js/portfolio.js
@@ -0,0 +1,40 @@
+function stickymenu(navbar, sticky) {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function getVal(type) {
+    z = type
+    if (z == 'text' || z == 'del') {
+    x = prompt("Enter Element:")
+    } else if (z == 'image' || z == 'delimg') {
+    temp = prompt("Enter Image URL:")
+    x = ''
+        for (var i = 0; i < temp.length; i++) {
+            if (temp[i] == '/') {
+                x += '^'
+            } else {
+                x += temp[i]
+            }
+        }
+    }
+
+    if (z == 'text' || z == 'image') {
+        y = prompt("Enter CSS:")
+    } else {
+        y = ''
+    }
+    n = '{{username}}'
+    const request = new XMLHttpRequest();
+    if (z == 'text') {
+        request.open('POST', `/process_info/${JSON.stringify( [x, y, z, n] )}`);
+    } else if (z == 'del') {
+        request.open('POST', `/process_info/${JSON.stringify( [x, y, z, n] )}`);
+    } else if (z == 'image' || z == 'delimg') {
+        request.open('POST', `/process_info/${JSON.stringify( [x, y, z, n] )}`);
+    }
+    request.send();
+}
